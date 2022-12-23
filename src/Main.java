81d0cdf6a3de7367d4ce5ab8d1c874240cbc8800
Kaerlaende <kaerlaende@mail.ru>
2022-12-23 14:37:45 +0300

    Дан массив пар названий книг и авторов, необходимо привести каждую пару в строку вида: «Название книги» И.О.Автора.

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String[] doubleAuthor = {"Михаил Булгаков", "Мастер и Маргарита", "Михаил Булгаков", "Собачье сердце", "Николай Гоголь", "Мёртвые души"};
+
+        for (int i = doubleAuthor.length-1; i >=0 ; i--) {
+            if (i % 2 != 0) {
+                System.out.print('"'+doubleAuthor[i] +'"'+ " ");
+            } else {System.out.print(doubleAuthor[i]+'.' + '\n' );}
+        }
+
+    }
+
+}
